4e8185a8cd16c492bd525956812173cde21eb412
Austin-Kho <kori.susie@gmail.com>
2023-08-03 14:49:16 +0900

    base url update

--- _config/urls.py
+++ _config/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from _config.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # shkim
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    path('api/', include('api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
