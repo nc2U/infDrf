aeb54243dba8d36a769febb1ef34b6f4dc447869
Austin-Kho <kori.susie@gmail.com>
2023-08-04 19:32:08 +0900

    api v2 update

--- apiV2/serializers.py
+++ apiV2/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from blog.models import Post
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        # fields = '__all__'
+        fields = ['id', 'title', 'image', 'like', 'category']
