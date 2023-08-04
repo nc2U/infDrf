5161ee495994c06e7473368ba8aaa21b4a5204da
Austin-Kho <kori.susie@gmail.com>
2023-08-04 18:21:44 +0900

    apiV2 url debug

--- apiV2/urls.py
+++ apiV2/urls.py
@@ -6,7 +6,6 @@ from apiV2.views import UserViewSet
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
-app_name = 'apiV2'
 urlpatterns = [
     path('', include(router.urls)),
 ]
